6e1b02a0036d00d4551e329e4d28f952f753eef6
Myrzaiym Keldibekova <myrzaiymkeldibekova1@gmail.com>
2024-03-23 16:15:38 +0600

    comment

---
+++ src/main/java/houserent/dto/SimpleResponse.java
@@ -0,0 +1,14 @@
+package houserent.dto;
+
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SimpleResponse {
+    private HttpStatus httpStatus;
+    private String message;
+}

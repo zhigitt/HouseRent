6010dbc5513254b865dd732d5af4d966ffb13d0d
Myrzaiym Keldibekova <myrzaiymkeldibekova1@gmail.com>
2024-03-26 13:28:55 +0600

    Like

---
+++ src/main/java/houserent/repository/LikeRepository.java
@@ -0,0 +1,7 @@
+package houserent.repository;
+
+import houserent.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
+}
